849c162252a5e72ddf8d1a4d773fb9fcb5598792
wwdzhang <wwdzhang@gmail.com>
2025-05-26 14:17:29 +0800

    [0000][Wei] init: init the spring boot project

---
+++ src/main/java/org/tw/agent_backend_demo/AgentBackendDemoApplication.java
@@ -0,0 +1,13 @@
+package org.tw.agent_backend_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentBackendDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgentBackendDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/tw/agent_backend_demo/AgentBackendDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tw.agent_backend_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentBackendDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
